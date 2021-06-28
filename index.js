12c6d333befa6cba020f137874d0c7e529e23045
Simo Siren <simo.siren@stud.pointcollege.fi>
2021-06-28 21:34:06 +0300

    started to build resolvers with mongodb

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const { ApolloServer, UserInputError, gql } = require('apollo-server')
 const mongoose = require('mongoose')
-//const Person = require('./models/person')
+const Author = require('./models/author')
+const Book = require('./models/book')
+
 const MONGODB_URI = require('./dbconn')
 
 console.log('connecting to', MONGODB_URI)
@@ -24,9 +26,8 @@ type Author {
   type Book {
     title: String!
     published: Int!
-    author: String!
-    id: ID!
     genres: [String!]!
+    id: ID!
   }
   type Query {
     bookCount: Int!
@@ -37,7 +38,6 @@ type Author {
   type Mutation {
     addBook(
         title: String!
-        author: String!
         published: Int!
         genres: [String!]!
     ): Book,
@@ -50,62 +50,25 @@ type Author {
 
 const resolvers = {
     Query: {
-        authorCount: () => authors.length,
-        bookCount: () => books.length,
-        allAuthors: () => {
-            return authors.map(author => {
-                const authorsBooks = books.filter(b => b.author === author.name)
-                return { ...author, bookCount: authorsBooks.length }
-            })
+        authorCount: () => Book.collection.countDocuments(),
+        bookCount: () => Book.collection.countDocuments(),
+        allAuthors: (root, args) => {
+            // filters missing
+            return Author.find({})
         },
-
         allBooks: (root, args) => {
-            if (args.author && args.genre) {
-                const filtered = books.filter(book => book.author === args.author)
-                return filtered.filter(b => b.genres.includes(args.genre))
-            }
-            else if (args.author) {
-                const filteredByAuthor = books.filter(book => book.author === args.author)
-                return filteredByAuthor
-            }
-            else if (args.genre) {
-                const filteredByGenre = books.filter(b => b.genres.includes(args.genre))
-                return filteredByGenre
-            }
-            else //If no parameter return all books
-                return books
+            // filters missing
+            return Book.find({})
         }
     },
     Mutation: {
         addBook: (root, args) => {
-            const book = { ...args, id: uuid() }
-            books = books.concat(book)
-
-            const existingAuthor = authors.find(a => a.name === args.author)
-            if (!existingAuthor) {
-                authors = authors.concat({
-                    name: args.author,
-                    id: uuid()
-                })
-            }
-            return book
+            const book = new Book({ ...args })
+            return book.save()
         },
-        editAuthor: (root, args) => {
-            console.log(...args)
-            const year = args.setBornTo
-            const authorToChange = authors.find(a => a.name === args.name)
-
-            if (!authorToChange) {
-                return null
-            }
-            authorToChange.born = year
-            authors = authors.map(a => {
-                return a.id === authorToChange.id ? authorToChange : a
-            })
-            return authorToChange
-        }
     }
 }
+
 
 const server = new ApolloServer({
     typeDefs,
