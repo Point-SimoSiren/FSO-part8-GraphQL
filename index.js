603f9c6b73cfcff621cb8a631d135390e3af0322
Simo Siren <simo.siren@stud.pointcollege.fi>
2021-06-28 10:21:45 +0300

    mongo connected n schemas done

--- index.js
+++ index.js
@@ -1,97 +1,18 @@
-const { ApolloServer, gql } = require('apollo-server')
+const { ApolloServer, UserInputError, gql } = require('apollo-server')
+const mongoose = require('mongoose')
+//const Person = require('./models/person')
+const MONGODB_URI = require('./dbconn')
 
-const { v1: uuid } = require('uuid')
+console.log('connecting to', MONGODB_URI)
 
-let authors = [
-    {
-        name: 'Robert Martin',
-        id: "afa51ab0-344d-11e9-a414-719c6709cf3e",
-        born: 1952
-    },
-    {
-        name: 'God almighty',
-        id: "yha54ab1-344b-11e9-a414-719c6733cf3e",
-        born: 0000
-    },
-    {
-        name: 'Martin Fowler',
-        id: "afa5b6f0-344d-11e9-a414-719c6709cf3e",
-        born: 1963
-    },
-    {
-        name: 'Fyodor Dostoevsky',
-        id: "afa5b6f1-344d-11e9-a414-719c6709cf3e",
-        born: 1821
-    },
-    {
-        name: 'Joshua Kerievsky', // birthyear not known
-        id: "afa5b6f2-344d-11e9-a414-719c6709cf3e",
-    },
-    {
-        name: 'Sandi Metz', // birthyear not known
-        id: "afa5b6f3-344d-11e9-a414-719c6709cf3e",
-    },
-]
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+    .then(() => {
+        console.log('connected to MongoDB')
+    })
+    .catch((error) => {
+        console.log('error connection to MongoDB:', error.message)
+    })
 
-
-let books = [
-    {
-        title: 'Clean Code',
-        published: 2008,
-        author: 'Robert Martin',
-        id: "afa5b6f4-344d-11e9-a414-719c6709cf3e",
-        genres: ['refactoring']
-    },
-    {
-        title: 'Agile software development',
-        published: 2002,
-        author: 'Robert Martin',
-        id: "afa5b6f5-344d-11e9-a414-719c6709cf3e",
-        genres: ['agile', 'patterns', 'design']
-    },
-    {
-        title: 'Holy Bible',
-        published: 0070,
-        author: 'God almighty',
-        id: "sta5b6f4-346h-11r9-a416-718c6719cf3s",
-        genres: ['life', 'salvation']
-    },
-    {
-        title: 'Refactoring, edition 2',
-        published: 2018,
-        author: 'Martin Fowler',
-        id: "afa5de00-344d-11e9-a414-719c6709cf3e",
-        genres: ['refactoring']
-    },
-    {
-        title: 'Refactoring to patterns',
-        published: 2008,
-        author: 'Joshua Kerievsky',
-        id: "afa5de01-344d-11e9-a414-719c6709cf3e",
-        genres: ['refactoring', 'patterns']
-    },
-    {
-        title: 'Practical Object-Oriented Design, An Agile Primer Using Ruby',
-        published: 2012,
-        author: 'Sandi Metz',
-        id: "afa5de02-344d-11e9-a414-719c6709cf3e",
-        genres: ['refactoring', 'design']
-    },
-    {
-        title: 'Crime and punishment',
-        published: 1866,
-        author: 'Fyodor Dostoevsky',
-        id: "afa5de03-344d-11e9-a414-719c6709cf3e",
-        genres: ['classic', 'crime']
-    },
-    {
-        title: 'The Demon ',
-        published: 1872,
-        author: 'Fyodor Dostoevsky',
-        id: "afa5de04-344d-11e9-a414-719c6709cf3e",
-        genres: ['classic', 'revolution']
-    },
-]
 
 const typeDefs = gql`
 type Author {
@@ -102,16 +23,16 @@ type Author {
   }
   type Book {
     title: String!
-    published: Int
+    published: Int!
     author: String!
     id: ID!
     genres: [String!]!
   }
   type Query {
-bookCount: Int!
-authorCount: Int!
-allAuthors: [Author!]! 
-allBooks(author: String, genre: String): [Book]
+    bookCount: Int!
+    authorCount: Int!
+    allAuthors: [Author!]! 
+    allBooks(author: String, genre: String): [Book]
   }
   type Mutation {
     addBook(
@@ -170,6 +91,7 @@ const resolvers = {
             return book
         },
         editAuthor: (root, args) => {
+            console.log(...args)
             const year = args.setBornTo
             const authorToChange = authors.find(a => a.name === args.name)
 
